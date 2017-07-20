55537d651ab1214a970ed525daa37f8fbd44966a
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-07-20 14:58:46 +0300

    I have added a dynamic route called foodz.It will give more information about the hotel the user has clicked

--- app/router.js
+++ app/router.js
@@ -8,7 +8,7 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('contact');
-  this.route('foodz');
+  this.route('foodz', {path: '/foodz/:hotel_id'});
 });
 
 export default Router;
