b7138a050023dc6b66bd4c65b7370d78942be615
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-07-21 14:44:40 +0300

    I have added a computed property which allows the name and rating to be displayed together with a new attribute called heading

--- app/components/review-tile.js
+++ app/components/review-tile.js
@@ -1,4 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  heading: Ember.computed('rating.author', 'rating.score', function(){
+    return this.get('rating.author') + ' - ' + this.get('rating.score');
+  }),
 });
